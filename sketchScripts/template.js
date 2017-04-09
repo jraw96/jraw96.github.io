6b4e13c66c8f1bd402d1902f5aa0fdf8ceb2404b
jraw@uwo.ca <jraw@uwo.ca>
2017-04-09 00:19:25 -0400

    Entire working file directory for the sketchPad Add

---
+++ sketchScripts/template.js
@@ -0,0 +1,15 @@
+//template.js
+
+function template(colour, initialSpotInCanvas) {
+    this.colour = colour;
+    this.initialSpotInCanvas = initialSpotInCanvas;
+}
+
+template.prototype.draw = function(mouse){
+};
+
+template.prototype.scan = function(mousePosition){
+}
+
+template.prototype.move = function(moveX,moveY){
+}
